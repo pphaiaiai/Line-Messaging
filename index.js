4f42a91d4ca76c35c55ae339a585d93631139303
Tarathep Siripis <tarathep.siripis@mail.kmutt.ac.th>
2023-02-16 16:16:17 +0700

    Add Get message and get data line oa and call extension

--- index.js
+++ index.js
@@ -17,17 +17,45 @@ const client = new line.Client(lineConfig);
 app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
     try {
         const events = req.body.events;
-        console.log("event ----------->", events);
-        return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send('OK');
-
+        if (events.length === 0) {
+            return await initial(events).then(() => res.status(200).send());
+        }
+        await Promise.all(events.map(item => execute(item)));
+        res.status(201).send();
     } catch (error) {
+        console.error(error);
         res.status(500).end();
     }
 });
 
-const handleEvent = async (event) => {
-    console.log("event ----------->", event);
-    return client.replyMessage(event.replyToken, {type: 'text', text: 'ควย'});
+const execute = async (event) => {
+    // console.log("event ----------->", event);
+    // return client.replyMessage(event.replyToken, {type: 'text', text: 'hehe'});
+
+    const Extension = require('../LINE_OA/index.js');
+    const newExtension = new Extension();
+
+    newExtension.initialization({
+        ACCESS_TOKEN: lineConfig.channelAccessToken,
+        storage_bucket_url: ''
+    })
+
+    newExtension.execute({
+        function_name: "getMessages",
+        execute_data: event
+    });
+}
+
+const initial = async (event) => {
+    // console.log("event ----------->", event);
+
+    const ExtensionInstaller = require('../LINE_OA/installer.js');
+    const newExtensionInstaller = new ExtensionInstaller();
+
+    newExtensionInstaller.initialization({
+        ACCESS_TOKEN: lineConfig.channelAccessToken,
+        storage_bucket_url: ''
+    })
 }
 
 app.listen(4000, () => {
